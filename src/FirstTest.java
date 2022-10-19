00b5f97e217a60b365ad8da7bfb8102cbe62d53c
Natalia Gamazenkova <gamazenkova.n@gmail.com>
2022-10-20 00:50:05 +0400

    add first android test code from lessons

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -33,8 +38,69 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest()
+    public void searchJava()
     {
-        System.out.println("First test run");
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"));
+        waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java");
+        waitForElementPresent(By.xpath("//*[contains(@text, 'Object-oriented')]"));
+    }
+
+    @Test
+    public void cancelSearch(){
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"));
+        waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java");
+        waitForElementAndClear(By.id("org.wikipedia:id/search_src_text"));
+        waitForElementAndClick(By.id("org.wikipedia:id/search_close_btn"));
+        waitForElementAbsence(By.id("org.wikipedia:id/search_close_btn"), "lol", 5);
+    }
+
+    @Test
+    public void compareArticleTitle(){
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"));
+        waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Java");
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Object-oriented')]"));
+        WebElement titleElement = waitForElementPresent(By.id("org.wikipedia:id/view_page_title_text"));
+        String articleTitle = titleElement.getText();
+
+        Assert.assertEquals(
+                "Java (programming language)",
+                articleTitle);
+
+    }
+
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + '\n');
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+    private WebElement waitForElementPresent(By by, String error_message){
+        return waitForElementPresent(by, error_message, 5);
+    }
+    private WebElement waitForElementPresent(By by){
+        return waitForElementPresent(by, "Can't find element by: " + "\n" + by, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by){
+        WebElement element = waitForElementPresent(by);
+        element.click();
+        return element;
+    }
+    private WebElement waitForElementAndSendKeys(By by, String value){
+        WebElement element = waitForElementPresent(by);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementAbsence(By by, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClear(By by){
+        WebElement element = waitForElementPresent(by);
+        element.clear();
+        return element;
     }
 }
